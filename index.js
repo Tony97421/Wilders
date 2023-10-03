6ace762eb2764081fc9a5b4e682ea98748f17ade
Techer Tony <techer307@gmail.com>
2023-10-03 10:52:24 +0200

    TEST chalk

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk"
+
+console.log(chalk.blue("Tony!"));
+console.log(chalk.red("Alexandra!"));
+console.log(chalk.green("Alexandre!"));
+console.log(chalk.yellow("Vincent!"));
